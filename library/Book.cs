eca381a4d718cf8d728ec931f8496f51eca99232
b00988631 <spiers-b3@ulster.ac.uk>
2025-10-06 11:45:07 +0100

    updated access code modifiers

---
+++ library/Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+        class Book
+        {
+            string Title;
+            string Author;
+            int ISBN;
+
+            public void displayInfo()
+            {
+                //output book information
+                Console.WriteLine($"Book title: {Title}");
+                Console.WriteLine($"Book Author: {Author}");
+                Console.WriteLine($"Book ISBN: {ISBN}\n");
+            }
+
+            public Book(string bookTitle, string bookAuthor, int bookISBN)
+            {
+                Title = bookTitle;
+                Author = bookAuthor;
+                ISBN = bookISBN;
+            }
+        }
+}
--- library/Program.cs
+++ library/Program.cs
@@ -1,25 +1,7 @@
-﻿class Book
-{
-    string Title;
-    string Author;
-    int ISBN;
+﻿
+using library;
 
-    void displayInfo()
-    {
-        //output book information
-        Console.WriteLine($"Book title: {Title}");
-        Console.WriteLine($"Book Author: {Author}");
-        Console.WriteLine($"Book ISBN: {ISBN}\n");
-    }
-
-    public Book(string bookTitle, string bookAuthor, int bookISBN)
-    {
-        Title = bookTitle;
-        Author = bookAuthor;
-        ISBN = bookISBN;
-    }
-
-    static void Main(string[] args)
+static void Main(string[] args)
     {
         //create new book object
         Book book = new Book("C# for beginners", "Bill Gates", 12345678);
@@ -32,4 +14,3 @@
         book2.displayInfo();
 
     }
-}
