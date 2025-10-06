b02bc870e0bfcc42890b4c829b3237230660f554
b00988631 <spiers-b3@ulster.ac.uk>
2025-10-06 12:27:45 +0100

    added member class

---
+++ library/Member.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+    internal class Member
+    {
+        private int memberId;
+        private string name;
+        private string address;
+        private int phone;
+
+         //make memberid read only
+         public int MemberId
+        {
+            get { return memberId; }
+            private set { memberId = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+        public int Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+
+        public void displayInfo()
+        {
+            //output book information
+            Console.WriteLine($"Member ID: {MemberId}");
+            Console.WriteLine($"Member Name: {Name}");
+            Console.WriteLine($"Member Address: {Address}");
+            Console.WriteLine($"Member Phone Number: {Phone}\n");
+        }
+
+        //constructor
+        public Member (int memberId, string name, string address, int phone)
+        {
+            this.MemberId = memberId;
+            this.Name = name;
+            this.Address = address;
+            this.Phone = phone;
+        }
+    }
+}
--- library/Program.cs
+++ library/Program.cs
@@ -14,5 +14,12 @@ class Program
         book.displayInfo();
         book2.displayInfo();
 
+        //create members
+        Member member = new Member(1, "John Smith", "1 High Street", 0790090090);
+        Member member2 = new Member(2, "Mary Jones", "102 Garden Street", 0790345666);
+
+        //output member information 
+        member.displayInfo();
+        member2.displayInfo();
     }
 }
