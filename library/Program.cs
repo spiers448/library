82c191095cf6ee8abef319773262f07f2ee1726c
b00988631 <spiers-b3@ulster.ac.uk>
2025-10-06 11:30:37 +0100

    added code

--- library/Program.cs
+++ library/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Book
+{
+    string Title;
+    string Author;
+    int ISBN;
+
+    void displayInfo()
+    {
+        //output book information
+        Console.WriteLine($"Book title: {Title}");
+        Console.WriteLine($"Book Author: {Author}");
+        Console.WriteLine($"Book ISBN: {ISBN}\n");
+    }
+
+    static void Main(string[] args)
+    {
+        //create new book object
+        Book book = new Book();
+
+        //book information
+        book.Title = "C# for beginners";
+        book.Author = "Bill Gates";
+        book.ISBN = 123456789;
+
+        //create 2nd book
+        Book book2 = new Book();
+
+        //2nd book information
+        book2.Title = "C# Methods and classes";
+        book2.Author = "Microsoft";
+        book2.ISBN = 987654321;
+        
+        //output book information
+        book.displayInfo();
+        book2.displayInfo();
+
+    }
+}
