cf31d0faed9e28af6546bc86b4a267929d39bd15
b00988631 <spiers-b3@ulster.ac.uk>
2025-11-03 15:06:43 +0000

    integrated lists

--- library/Program.cs
+++ library/Program.cs
@@ -4,22 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        //create new book object
-        Book book = new Book("C# for beginners", "Bill Gates", 12345678);
+        //create lists
+        List<Book> books = new List<Book>();
+        List<Member> members = new List<Member>();
 
-        //create 2nd book
-        Book book2 = new Book("C# Methods and classes", "Microsoft", 987654321);
+        //create new book objects for list
+        books.Add(new Book("C# for beginners", "Bill Gates", 12345678));
+        books.Add(new Book("C# Methods and classes", "Microsoft", 987654321));
 
-        //output book information
-        book.displayInfo();
-        book2.displayInfo();
+        //create new member objects for list
+        members.Add(new Member(1, "John Smith", "1 High Street", 0790090090));
+        members.Add(new Member(2, "Mary Jones", "102 Garden Street", 0790345666));
 
-        //create members
-        Member member = new Member(1, "John Smith", "1 High Street", 0790090090);
-        Member member2 = new Member(2, "Mary Jones", "102 Garden Street", 0790345666);
+        //display members in members list
+        foreach (Member Member in members)
+        {
+            Member.displayInfo();
+        }
 
-        //output member information 
-        member.displayInfo();
-        member2.displayInfo();
+        //display books in books list
+        foreach (Book Book in books)
+        {
+            Book.displayInfo();
+        }
     }
 }
